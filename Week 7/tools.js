eb32e56673ef03cbb2c7780dde790bf3ca4230fa
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-04-22 20:01:58 +0700

    Week 7

--- Week 7/tools.js
+++ Week 7/tools.js
@@ -1,10 +1,23 @@
-function calculatePrice(){
+function calculatePrice() {
     alert("Calculate Price");
 }
 
-function calculatePrice2(){
+function calculatePrice2() {
     alert("Calculate Price 2");
+    event.stopImmediatePropagation();
+}
+
+function clickP3() {
+    alert("P3 Clicked");
 }
 
 var myButton = document.getElementById("price2");
 myButton.onclick = calculatePrice2;
+myButton.onclick = calculatePrice;
+
+var myButton = document.getElementById("price3");
+myButton.addEventListener("click", calculatePrice2);
+myButton.addEventListener("click", calculatePrice);
+
+var myP3 = document.getElementById("p3");
+myP3.addEventListener("click", clickP3);
