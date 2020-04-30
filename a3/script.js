1f55d0b9076fa506e9e3286567b7f324d23e67ee
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-04-30 20:19:40 +0700

    Remove Regex From JS, Update HTML Pattern for Customer Info

--- a3/script.js
+++ a3/script.js
@@ -22,65 +22,30 @@ function stickyNavBar() {
     }
 }
 
-function checkInputName() {
-    var patt = /^[A-Za-z]+$/;
-    name = document.getElementById("cust-name").value;
-    if (name.match(patt))
-        return true;
-    else
-        alert("Please Enter A Valid Name");
-    return false;
-}
-
-function checkInputEmail() {
-    var patt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    email = document.getElementById("cust-email").value;
-    if (email.match(patt))
-        return true;
-    else
-        alert("Please Enter A Valid Email");
-    return false;
-}
-
-function checkInputMobile() {
-    var patt = /^([+61|0](2|4|3|7|8|)){0,2}([ 0-9]|[(]){2,3}([)]|[0-9]){6}([ ])[0-9]{7,20}$/;
-    mobile = document.getElementById("cust-mobile").value;
-    if (mobile.match(patt))
-        return true;
-    else
-        alert("Please Enter A Valid Phone Number");
-    return false;
-}
-
-function checkInputCreditCard() {
-    var card = /[0-9]{14,19}/;
-    credit = document.getElementById("cust-credit").value;
-    if (credit.match(visa))
-        return true;
-    else
-        alert("Please Enter A Valid Credit Card");
-    return false;
-}
-
 // Expiry Date Validation
 const form = document.getElementById('form');
 const expiryMonth = document.getElementById('expiryMonth');
 const expiryYear = document.getElementById('expiryYear');
 
-// form.addEventListener('submit', ev => {
-//     ev.preventDefault();
+form.addEventListener('submit', ev => {
+    ev.preventDefault();
 
-//     const month = expiryMonth.value;
-//     const year = expiryYear.value;
+    const month = expiryMonth.value;
+    const year = expiryYear.value;
 
-//     const expiryDate = new Date(year + '-' + month + '-01');
+    // Create a date object from month and year, on the first
+    // of that month.
+    const expiryDate = new Date(year + '-' + month + '-01');
 
-//     if (expiryDate < new Date()) {
-//         console.log('fail')
-//     } else {
-//         console.log('pass')
-//     }
-// })
+    // You can compare date objects, this says if the expiryDate is
+    // less than todays date, i.e. in the past.
+    if (expiryDate < new Date()) {
+        // Fails validation, show some error message to user
+        console.log('fail')
+    } else {
+        console.log('pass')
+    }
+})
 
 function selectionSTA() {
     var selector = document.getElementById('seats[STA]');
