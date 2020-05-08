04bae80b8c9ac2172c091164050af24e67fa5f9c
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-05-08 09:11:33 +0700

    Week 9 Lab

--- Week 9 Lab/form.php
+++ Week 9 Lab/form.php
@@ -6,9 +6,37 @@
     </style>
 </head>
 <body>
+<?php
+function test_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+    if ($_SERVER["required_method"] == "POST"){
+        if (empty($_POST["name"])){
+            $nameErr = "Name is required";
+        }    else {
+            $name = test_input($_POST["name"]);
+            if (!preg_match("/^[a-zA-Z]*$/ ",$name )) {
+                $nameErr = "Only letters and white space are allowed.";
+            }
+        }
+        if (emty($_POST["email"])){
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $emailErr = "Invalid email format";
+            }
+        }
+    }
+?>
+
+
     <h2>PHP Form Validation Example</h2>
     <p><span class="error">* required field</span></p>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         Name: <input type="text" name= "name">
         <p><span class="error">* <?php echo $nameErr; ?></span></p
         <br><br>
