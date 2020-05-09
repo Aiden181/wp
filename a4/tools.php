409b2427f56c9652b9202bc4493c83800001df5f
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-05-09 21:45:48 +0700

    name - email - mobile validation php

--- a4/tools.php
+++ a4/tools.php
@@ -19,5 +19,19 @@ if (isset($_POST['session-reset'])) {
 
 }
 
+$name = test_input($_POST["cust_name"]);
+if (!preg_match("/^[A-Za-z]+$/",$name)) {
+  $nameErr = "Please enter a valid name";
+}
+
+$email = test_input($_POST["cust_email"]);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $emailErr = "Invalid email format";
+}
+
+$mobile = test_input($_POST["cust_mobile"]);
+if (!preg_match("/^(?:\+?(61))? ?(?:\((?=.*\)))?(0?[2-57-8])\)? ?(\d\d(?:[- ](?=\d{3})|(?!\d\d[- ]?\d[- ]))\d\d[- ]?\d[- ]?\d{3})$/",$mobile)) {
+  $mobileErr = "Please enter a valid mobile number";
+}
 
 ?>
