642d3cfb8d922c1592901f5aa3f883ec57a3f5df
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-05-09 16:21:19 +0700

    tools.php Update

--- a4/tools.php
+++ a4/tools.php
@@ -3,4 +3,19 @@
 
 // Put your PHP functions and modules here
 
+// Print data and shape/structure of data
+function preShow( $arr, $returnAsString=false ) {
+  $ret  = '<pre>' . print_r($arr, true) . '</pre>';
+  if ($returnAsString)
+       return $ret;
+ else 
+      echo $ret; 
+}
+// Reset the session - Submit Button
+if (isset($_POST['session-reset'])) {
+  foreach($_SESSION as $something => &$whatever) {
+       unset($whatever);
+  }
+
+}
 ?>
