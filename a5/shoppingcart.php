8cebb6ebc438a43d450bbc57ac67bf91501dfbb6
s3757317 <s3757317@users.noreply.github.com>
2020-05-22 01:51:30 +0700

    make shopping cart css code a standalone file

--- a5/shoppingcart.php
+++ a5/shoppingcart.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.php">
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/shoppingcartstyle.php">
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
