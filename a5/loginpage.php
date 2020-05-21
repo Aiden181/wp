2493ef10f63bb6d0ce43e0b10258b3057c84af8c
s3757317 <s3757317@users.noreply.github.com>
2020-05-22 02:29:35 +0700

    removed redundant code and forgot password

--- a5/loginpage.php
+++ a5/loginpage.php
@@ -13,31 +13,18 @@
     <title>Login</title>
 </head>
 <body>
-    
     <div class="wrapper fadeInDown" id="bg-image">
         <div id="formContent">
           <!-- Tabs Titles -->
           <h2 class="active"> Sign In </h2>
-      
-          <!-- Icon -->
-          <div class="fadeIn first">
-            <img 
-          </div>
-      
+
           <!-- Login Form -->
           <form>
             <input type="text" id="login" class="fadeIn second" name="login" placeholder="Login" pattern="^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$" required> <!-- Username is 8-20 characters long -->
             <input type="password" id="password" class="fadeIn third" name="login" placeholder="Password" pattern=""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$ required> <!-- Minimum eight characters, at least one letter and one number -->
             <input type="submit" class="fadeIn fourth" value="Log In">
           </form>
-      
-          <!-- Remind Passowrd -->
-          <div id="formFooter">
-            <a class="underlineHover" href="#" style="color: #1e1e1e;">Forgot Password?</a>
-          </div>
-      
         </div>
       </div>
-
 </body>
 </html>
