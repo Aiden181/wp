955731f461c622051fd1631151c1d357e8edb12b
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-05-21 14:25:09 +0700

    Shopping Cart Page Update

---
+++ a5/shoppingcart.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.php">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>Cart</title>
+</head>
+<body>
+  <?php
+      include('includes/header.php');
+  ?>
+
+  <?php
+      include('includes/footer.php');
+      include('includes/javascript.php');
+  ?>
+</body>
+</html>
