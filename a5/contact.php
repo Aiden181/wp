b39487d06c7ee9ce6ef1db508776443e4188fb8b
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-05-21 17:11:35 +0700

    Contact Form Update

--- a5/contact.php
+++ a5/contact.php
@@ -22,6 +22,26 @@
       include('includes/header.php');
   ?>
 
+    <div class="container">
+    <form action="/action_page.php">
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="lname">Email</label>
+        <input type="email" id="email" name="email" placeholder="Enter your email..">
+
+        <label for="subject">Message</label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+        <input type="submit" value="Submit">
+    </form>
+    </div>
+
+    <div id="blankspace"></div>
+
   <?php
       include('includes/footer.php');
       include('includes/javascript.php');
--- a5/css/style.php
+++ a5/css/style.php
@@ -618,3 +618,31 @@ ul {
  
  
 /* End Of Shopping Cart */ 
+
+/* Contact Us Page */ 
+input[type=text],[type=email], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #e04b11;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f6f6f6;
+  padding: 20px;
+}
+/* End Of Contact Us Page */ 
