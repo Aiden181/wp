cedffa5bc82cec303495cbb6b63a226e0a2bf89a
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-05-22 16:56:41 +0700

    DW Product Page

---
+++ a5/products/dw1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link id='stylecss' type="text/css" rel="stylesheet" href="../css/style.php">
+    <link id='stylecss' type="text/css" rel="stylesheet" href="../css/productpagestyle.php">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>CLASSIC DOVER</title>
+</head>
+<body>
+    <?php
+        include('includes/header.php');
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <?php
+                $images = array();
+                array_push($images, 
+                "../img/watches/dw1.jpg", 
+                "../img/watches/dw1-1.png", 
+                "../img/watches/dw1-2.png", 
+                "../img/watches/dw1-3.png", 
+                "../img/watches/dw1-4.png");
+
+                $name = "CLASSIC DOVER";
+                $price = "179";
+                $caseSize = "36mm";
+                $caseThickness = "6mm";
+                $glass = "Hardened Mineral Glass";
+                $movement = "Japanese Quartz Movement";
+
+                include('includes/productdetails.php')
+            ?>
+        </div>
+    </div>
+
+    <div id="blankspace"></div>
+
+    <?php
+        include('includes/footer.php');
+    ?>
+</body>
+</html>
---
+++ a5/products/dw7.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link id='stylecss' type="text/css" rel="stylesheet" href="../css/style.php">
+    <link id='stylecss' type="text/css" rel="stylesheet" href="../css/productpagestyle.php">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>CLASSIC CANTERBURY</title>
+</head>
+<body>
+    <?php
+        include('includes/header.php');
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <?php
+                $images = array();
+                array_push($images, 
+                "../img/watches/dw7.jpg", 
+                "../img/watches/dw7-1.png", 
+                "../img/watches/dw7-2.png", 
+                "../img/watches/dw7-3.png", 
+                "../img/watches/dw7-4.png");
+
+                $name = "CLASSIC CANTERBURY";
+                $price = "199";
+                $caseSize = "36mm - 40mm";
+                $caseThickness = "6mm";
+                $glass = "Hardened Mineral Glass";
+                $movement = "Japanese Quartz Movement";
+
+                include('includes/productdetails.php')
+            ?>
+        </div>
+    </div>
+
+    <div id="blankspace"></div>
+
+    <?php
+        include('includes/footer.php');
+    ?>
+</body>
+</html>
