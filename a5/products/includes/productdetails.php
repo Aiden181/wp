276e7986bb97ee72bd5f887ce0b0cdab1f7853f0
s3757317 <s3757317@users.noreply.github.com>
2020-05-22 20:20:03 +0700

    added add item name to cart session function

--- a5/products/includes/productdetails.php
+++ a5/products/includes/productdetails.php
@@ -47,7 +47,67 @@
 
     <br>
     <br>
+
+    <?php
+        // get current page path from watches.csv
+        $pagePath = "";
+
+        // open watches.csv
+        $filename = "../watches.csv";
+        $file = fopen($filename, "r") or die("Unable to open file!");;
+        flock($file, LOCK_SH);
     
-    <button class="add-to-cart" onclick="addToCart()">Add To Cart</button>
+        // read the heading
+        $headings = fgetcsv($file);
+
+        // read through the lines and store each line as array element
+        while ($aLineOfCells = fgetcsv($file)) {
+            $tempList[] = $aLineOfCells;
+        }
+        
+        flock($file, LOCK_UN);
+        fclose($file);
+
+        // if name in array matches $name, assign path to $pagePath
+        foreach ($tempList as $watch) {
+            if ($watch[1] === $name) {
+                $pagePath = $watch[5];
+                break;
+            }
+        }
+    ?>
+
+    <form action="<?php echo $pagePath ?>" method="GET">
+        <button name='session-reset' value='Reset the session' onclick="this.form.submit()">Reset the session</button>
+        <button class="add-to-cart" name="item" onclick="this.form.submit()" value="<?php echo $name ?>">Add To Cart</button>
+    </form>
+
+    <?php
+        session_start();
+        // initialize session array
+        if (empty($_SESSION)) {
+            $_SESSION['cart'] = array();
+            if (empty($_SESSION['cart'])) {
+                $_SESSION['cart'] = array();
+            }
+        }
+        // when user clicks add to cart
+        if (isset($_GET['item'])) {
+            // add item to cart
+            array_push($_SESSION['cart'], $_GET['item']);
+
+            // redirect to page to prevent continuously pushing when refresh page
+            header("Location:" . $pagePath);
+            exit();
+        } else if (isset($_GET['session-reset'])) {
+            unset($_SESSION['cart']);
+            foreach($_SESSION as $something => &$whatever) {
+                unset($whatever);
+            }
+        }
+        
+        // preShow($_GET);
+        // preShow($_SESSION);
+    ?>
     </div>
 </div>
