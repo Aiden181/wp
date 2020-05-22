7cf593d2fca214a568d4dc5ad5266f4e726623ec
s3757317 <s3757317@users.noreply.github.com>
2020-05-22 17:44:21 +0700

    added redirection to product detail page to images

--- a5/index.php
+++ a5/index.php
@@ -102,168 +102,168 @@
       <!-- Push down content on small screens -->
       <div class="w3-hide-large" style="margin-top:83px"></div>
 
+        <br>
+        <br>
+        <br>
+        <br>
+        
+        <div id="carouselContainer">
+          <h1>NEW 2020 ARRIVALS</h1>
+          <!-- Carousel -->
+          <div id="newArrivalCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#newArrivalCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#newArrivalCarousel" data-slide-to="1"></li>
+              <li data-target="#newArrivalCarousel" data-slide-to="2"></li>
+            </ol>
 
-  <br>
-  <br>
-  <br>
-  <br>
-  
-  <div id="carouselContainer">
-    <h1>NEW 2020 ARRIVALS</h1>
-    <!-- Carousel -->
-    <div id="newArrivalCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#newArrivalCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#newArrivalCarousel" data-slide-to="1"></li>
-        <li data-target="#newArrivalCarousel" data-slide-to="2"></li>
-      </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="img/banner.jpg" alt="Banner">
+                <div class="w3-display-topleft w3-text-white" style="padding: 24px 48px">
+                  <p><a href="sevenfriday.php" class="w3-button w3-black w3-padding-large w3-large shopnow-btn-left">SHOP NOW</a></p>
+                </div>
+              </div>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="img/banner.jpg" alt="Banner">
-          <div class="w3-display-topleft w3-text-white" style="padding: 24px 48px">
-            <p><a href="sevenfriday.php" class="w3-button w3-black w3-padding-large w3-large shopnow-btn-left">SHOP NOW</a></p>
+              <div class="item">
+                <img src="img/banner1.png" alt="Banner1">
+                <div class="w3-display-topleft w3-text-white" style="padding: 24px 48px">
+                  <a href="movado.php" class="w3-button w3-black w3-padding-large w3-large shopnow-btn-right">SHOP NOW</a>
+                </div>
+              </div>
+
+              <div class="item">
+                <img src="img/banner2.png" alt="Banner2">
+                <div class="w3-display-topleft w3-text-white" style="padding: 24px 48px">
+                  <p><a href="mvmt.php" class="w3-button w3-black w3-padding-large w3-large shopnow-btn-left">SHOP NOW</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#newArrivalCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#newArrivalCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
         </div>
 
-        <div class="item">
-          <img src="img/banner1.png" alt="Banner1">
-          <div class="w3-display-topleft w3-text-white" style="padding: 24px 48px">
-            <a href="movado.php" class="w3-button w3-black w3-padding-large w3-large shopnow-btn-right">SHOP NOW</a>
+        <br>
+        <br>
+        <br>
+
+        <h1 class="watch-showcase-container" style="position: relative; left: 35px">FEATURED WATCHES</h1>
+        
+        <br>
+
+        <div class="w3-row watch-showcase-container">
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/movado2.php><img src="img/watches/movado2.png" style="width:100%"></a>
+                <span class="w3-tag w3-display-topleft">New</span>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">MODERN 47<br><b class="w3-text-red">$695.00</b></p>
+            </div>
+          </div>
+
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/sevenfriday3.php><img src="img/watches/sevenfriday3.png" style="width:100%"></a>
+                <span class="w3-tag w3-display-topleft">New</span>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">M3/04 – PINKY<br><b class="w3-text-red">$1,750.00</b></p>
+            </div>
+          </div>
+
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/sevenfriday1.php><img src="img/watches/sevenfriday1.png" style="width:100%"></a>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">P3B/06 RACING TEAM RED<br><b class="w3-text-red">$1,150.00</b></p>
+            </div>
+          </div>
+
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/mvmt3.php><img src="img/watches/mvmt3.jpg" style="width:100%"></a>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">CLASSIC BLACK ROSE<br><b class="w3-text-red">$110.00</b></p>
+            </div>
+          </div>
+          
+          <div class="w3-col l3 s6">
+          <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/dw7.php><img src="img/watches/dw7.jpg" style="width:100%"></a>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">CLASSIC CAMBRIDGE<br><b class="w3-text-red">$199.00</b></p>
+            </div>
+          </div>
+
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/dw6.php><img src="img/watches/dw6.jpg" style="width:100%"></a>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">CLASSIC GLASGOW<br><b class="w3-text-red">$199.00</b></p>
+            </div>
+          </div>
+
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/movado4.php><img src="img/watches/movado4.png" style="width:100%"></a>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">MOVADO ULTRA SLIM<br><b class="w3-text-red">$895.00</b></p>
+            </div>
+          </div>
+          
+          <div class="w3-col l3 s6">
+            <div class="w3-container">
+              <div class="w3-display-container">
+                <a href=products/sevenfriday7.php><img src="img/watches/sevenfriday7.png" style="width:100%"></a>
+                <div class="w3-display-hover" style="position: absolute; top: 75%; left: 25%;">
+                  <button class="w3-button w3-black">Add To Cart <i class="fa fa-shopping-cart"></i></button>
+                </div>
+              </div>
+              <p style="text-align: center;">M2/02<br><b class="w3-text-red">$1,474.00</b></p>
+            </div>
           </div>
         </div>
 
-        <div class="item">
-          <img src="img/banner2.png" alt="Banner2">
-          <div class="w3-display-topleft w3-text-white" style="padding: 24px 48px">
-            <p><a href="mvmt.php" class="w3-button w3-black w3-padding-large w3-large shopnow-btn-left">SHOP NOW</a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#newArrivalCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#newArrivalCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </div>
-
-  <br>
-  <br>
-  <br>
-
-  <h1 class="watch-showcase-container" style="position: relative; left: 35px">FEATURED WATCHES</h1>
-  <br>
-
-  <div class="w3-row watch-showcase-container">
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/movado2.png" style="width:100%">
-          <span class="w3-tag w3-display-topleft">New</span>
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">MODERN 47<br><b class="w3-text-red">$695.00</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/sevenfriday3.png" style="width:100%">
-          <span class="w3-tag w3-display-topleft">New</span>
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">M3/04 – PINKY<br><b class="w3-text-red">$1,750.00</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/sevenfriday1.png" style="width:100%">
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">P3B/06 RACING TEAM RED<br><b class="w3-text-red">$1,150.00</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/mvmt3.jpg" style="width:100%">
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 70px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">CLASSIC BLACK ROSE<br><b class="w3-text-red">$110.00</b></p>
-      </div>
-    </div>
-    
-    <div class="w3-col l3 s6">
-     <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/dw7.jpg" style="width:100%">
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">CLASSIC CAMBRIDGE<br><b class="w3-text-red">$199.00</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/dw6.jpg" style="width:100%">
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">CLASSIC GLASGOW<br><b class="w3-text-red">$199.00</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/movado4.png" style="width:100%">
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">MOVADO ULTRA SLIM<br><b class="w3-text-red">$895.00</b></p>
-      </div>
-    </div>
-    
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="img/watches/sevenfriday7.png" style="width:100%">
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black" style="position: relative; top: 60px;">Add To Cart <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p style="text-align: center;">M2/02<br><b class="w3-text-red">$1,474.00</b></p>
-      </div>
-    </div>
-  </div>
-
-  <div id="blankspace"></div>
+        <div id="blankspace"></div>
 
   <?php
     include('includes/footer.php');
