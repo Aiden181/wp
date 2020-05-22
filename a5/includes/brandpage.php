356b62735f8a6dd8447890cd3cf4cad18b51fd2c
s3757317 <s3757317@users.noreply.github.com>
2020-05-22 17:44:59 +0700

    design improvement and added viewing product count

--- a5/includes/brandpage.php
+++ a5/includes/brandpage.php
@@ -1,5 +1,24 @@
 <!-- Sorting selection -->
+<div style="height: 130px; background-color: rgba(210, 210, 210, 1)">
+    <div class="container">
+        <?php
+            $text = "";
+            if ($brandName === "sevenfriday") {
+                $text = "Seven Friday";
+            } else if ($brandName === "movado") {
+                $text = "Movado";
+            } else {
+                $text = $brandName;
+            }
+            echo "<h1 style=\"text-transform: uppercase\">$text</h1>"
+        ?>
+    </div>
+</div>
+
+<br>
+
 <div class="container">
+    <h4 style="position: relative; top: 35px; display: inline">Viewing <?php echo countWatches($brandName) ?> products</h4>
     <!-- note: since Daniel Wellington page is dw, 
     if the brand name is Daniel Wellington, then 
     the page will be dw.php, otherwise, continue like normal -->
