e6099d1494e51c4bd9e177c219ebbe2960066d77
s3757317 <s3757317@users.noreply.github.com>
2020-05-24 01:07:27 +0700

    fixed URL not found error on MVMT page

--- a5/includes/brandpage.php
+++ a5/includes/brandpage.php
@@ -19,10 +19,21 @@
 
 <div class="container">
     <h4 style="position: relative; top: 35px; display: inline">Viewing <?php echo countWatches($brandName) ?> products</h4>
-    <!-- note: since Daniel Wellington page is dw, 
-    if the brand name is Daniel Wellington, then 
-    the page will be dw.php, otherwise, continue like normal -->
-    <form action="<?php echo ($brandName === "Daniel Wellington" ? "dw" : $brandName) . ".php" ?>" method="GET">
+
+    <!----------------------------- FILTER ----------------------------->
+    <!-- since Daniel Wellington page is dw,  if the brand name is 
+    Daniel Wellington, then  the page will be dw.php, and if the brand 
+    name is MVMT, make the page name undercase to prevent URL not found 
+    error,  otherwise, continue like normal -->
+    <form action="<?php
+     if ($brandName === "Daniel Wellington") {
+        echo "dw" . ".php";
+    } else if ($brandName === "MVMT") {
+        echo "mvmt" . ".php";
+    } else {
+        echo "$brandName" . ".php";
+    }
+    ?>" method="GET">
         <select name="orderby" class="sort" onchange="this.form.submit()">
             <option <?php keepSelectFieldAfterSubmit('none'); ?> value="none" selected="selected">Default</option>
             <option <?php keepSelectFieldAfterSubmit('price-asc'); ?> value="price-asc">Price: Low to High</option>
@@ -31,6 +42,7 @@
             <option <?php keepSelectFieldAfterSubmit('name-desc'); ?> value="name-desc">Name: Descending</option>
         </select>
     </form>
+    <!------------------------- END OF FILTER ------------------------->
 </div>
 
 <br>
