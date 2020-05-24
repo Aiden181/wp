75f69a6abd8e05ebdd3054d70dbcdbed062827f0
s3757317 <s3757317@users.noreply.github.com>
2020-05-25 03:40:02 +0700

    removed test code

--- a5/products/includes/productdetails.php
+++ a5/products/includes/productdetails.php
@@ -1,20 +1,3 @@
-<?php
-	$images = array();
-	array_push($images, 
-	"../img/watches/dw1.jpg", 
-	"../img/watches/dw1-1.png", 
-	"../img/watches/dw1-2.png", 
-	"../img/watches/dw1-3.png", 
-	"../img/watches/dw1-4.png");
-
-	$name = "CLASSIC DOVER";
-	$price = "179";
-	$caseSize = "36mm";
-	$caseThickness = "6mm";
-	$glass = "Hardened Mineral Glass";
-	$movement = "Japanese Quartz Movement";
-?>
-
 <?php include('includes/header.php'); ?>
 
 <div class="container">
