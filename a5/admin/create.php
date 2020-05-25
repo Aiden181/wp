ecfe9bc8b8f83d52457df0281911354f002e3003
s3757317 <s3757317@users.noreply.github.com>
2020-05-25 21:08:49 +0700

    can upload multiple images

--- a5/admin/create.php
+++ a5/admin/create.php
@@ -11,47 +11,48 @@ require_once "../includes/database.php";
 // Define variables and initialize with empty values
 $id = $brand = $prodname = $img1 = $img2 = $img3 = $img4 = $img4 = "";
 $price = 0.00;
-$id_err = $brand_err = $prodname_err = $price_err = $img_err = "";
+$id_err = $brand_err = $prodname_err = $price_err = "";
+$img_err = array();
 
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "GET") {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate ID
-    if (isset($_GET["id"])) {
-        $input_id = trim($_GET["id"]);
-        if(empty($input_id)){
-            $id_err = "Please enter product id.";     
+    if (isset($_POST["id"])) {
+        $input_id = trim($_POST["id"]);
+        if(empty($input_id)) {
+            $id_err = "Please enter product id.";
         } else{
             $id = $input_id;
         }
     }
     
-     // Validate Brand Name
-    if (isset($_GET["brand"])) {
-        $input_brand = trim($_GET["brand"]);
-        if(empty($input_brand)){
-            $brand_err = "Please enter product brand.";     
+    // Validate Brand Name
+    if (isset($_POST["brand"])) {
+        $input_brand = trim($_POST["brand"]);
+        if(empty($input_brand)) {
+            $brand_err = "Please enter product brand.";
         } else{
             $brand = $input_brand;
         }
     }
-    
+
     // Validate Product Name
-    if (isset($_GET["prodname"])) {
-        $input_prodname = trim($_GET["prodname"]);
-        if(empty($input_address)){
-            $prodname_err = "Please enter product name.";     
+    if (isset($_POST["prodname"])) {
+        $input_prodname = trim($_POST["prodname"]);
+        if(empty($input_prodname)) {
+            $prodname_err = "Please enter product name.";
         } else{
             $prodname = $input_prodname;
         }
     }
     
     // Validate Price
-    if (isset($_GET["price"])) {
-        $input_price = trim($_GET["price"]);
-        if(empty($input_salary)){
-            $price_err = "Please enter the price for your product.";     
-        } else if(!ctype_digit($input_salary)){
+    if (isset($_POST["price"])) {
+        $input_price = trim($_POST["price"]);
+        if(empty($input_price)) {
+            $price_err = "Please enter the price for your product.";
+        } else if(!ctype_digit($input_price)) {
             $price_err = "Please enter a positive integer value.";
         } else{
             $price = $input_price;
@@ -59,62 +60,76 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
     }
 
     // Validate Image
-    if (isset($_GET["img"])) {
-        $input_img = ($_GET["img"]);
-        if(empty($input_salary)){
-            $img_err = "Please upload an image.";  
+    if (isset($_POST["submit"])) {
+        // no images uploaded
+        if($_FILES["files"]['tmp_name']['0'] === "") {
+            array_push($img_err, "Please upload an image.");
         }
-        $target_dir = "img/watches";
-        $target_file = $target_dir . basename($_FILES["img"]["name"]);
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        $uploadOk = true;
+        // image(s) uploaded
+        else {
+        foreach($_FILES["files"]["tmp_name"] as $key => $tmp_name) {
 
-        // Check if image file is a actual image or fake image
-        if(isset($_GET["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = true;
+            // assign array values to intuitive variable
+            $fileName = $_FILES["files"]["name"][$key];
+            $fileTemp = $_FILES["files"]["tmp_name"][$key];
+            $fileSize = $_FILES["files"]["size"][$key];
+
+            // specifies the directory where the file is going to be placed
+            $target_dir = "../img/watches/";
+            // specifies the path of the file to be uploaded
+            $target_file = $target_dir . basename($fileName);
+            // holds the file extension of the file (in lower case)
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            // set upload failure or success
+            $uploadOk = 1;
+
+            // Check if image file is a actual image or fake image
+            $check = getimagesize($fileTemp);
+            if($check !== 0) {
+                // array_push($img_err, "File is an image - " . $check["mime"] . ".");
+                $uploadOk = 1;
             } else {
-                echo "File is not an image.";
-                $uploadOk = false;
+                array_push($img_err, "File is not an image.");
+                $uploadOk = 0;
+            }
+
+            // Check if file already exists
+            if (file_exists($target_file)) {
+                array_push($img_err, "File already exists!");
+                $uploadOk = 0;
+            }
+            // Check file size (disallow files larger than 3 MegaBytes or 3,000,000 Bytes)
+            if ($fileSize > 3000000) {
+                array_push($img_err, "Your file is too large!");
+                $uploadOk = 0;
+            }
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                array_push($img_err, "Only JPG, JPEG, and PNG files are allowed!");
+                $uploadOk = 0;
+            }
+            // Check if $uploadOk is set to false by an error
+            if ($uploadOk == 0) {
+                array_push($img_err, "Sorry, your file was not uploaded.");
+            // if everything is ok, upload file
+            } else {
+                if (move_uploaded_file($fileTemp, $target_file)) {
+                    array_push($img_err, "The file ". basename($fileName). " has been uploaded successfully.");
+                } else {
+                    array_push($img_err, "There was an error uploading your file.");
+                }
             }
         }
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
-            $uploadOk = false;
-        }
-        // Check file size
-        if ($_FILES["img"]["size"] > 500000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = false;
-        }
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-            echo "Sorry, only JPG, JPEG, PNG files are allowed.";
-            $uploadOk = false;
-        }
-        // Check if $uploadOk is set to false by an error
-        if (!$uploadOk) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, upload file
-        } else {
-            if (move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["img"]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
         }
     }
 
     
     // Check input errors before inserting in database
-    if(empty($brand_err) && empty($prodname_err) && empty($price_err)){
+    if(empty($brand_err) && empty($prodname_err) && empty($price_err)) {
         // Prepare an insert statement
         $sql = "INSERT INTO products (brand, prodname, price) VALUES (?, ?, ?)";
 
-        if($stmt = mysqli_prepare($conn, $sql)){
+        if($stmt = mysqli_prepare($conn, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, $param_id, $param_brand, $param_prodname, $param_price);
             
@@ -125,7 +140,7 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
             $param_price = $price;
             
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if(mysqli_stmt_execute($stmt)) {
                 // Records created successfully. Redirect to landing page
                 header("location: manage.php");
                 exit();
@@ -164,13 +179,13 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
 <body>
     <div class="wrapper">
         <div class="container-fluid">
-        <button type="button" class="btn" style="position: relative; right: 30%; top: 70px ;background-color: #e04b11; color:"><a href="manage.php" style="color:white">Manage</a></button>
+        <button type="button" class="btn" style="position: relative; right: 30%; top: 70px; background-color: #e04b11; color:"><a href="manage.php" style="color:white">Manage</a></button>
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
                         <h2>Add New Product</h2>
                     </div>
-                    <form action="create.php" method="GET">
+                    <form action="create.php" method="post" enctype="multipart/form-data">
                         <div class="form-group <?php echo (!empty($id_err)) ? 'has-error' : ''; ?>">
                             <label>Product ID</label>
                             <input type="text" name="id" class="form-control" value="<?php echo $id; ?>">
@@ -191,16 +206,16 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
                             <input type="number" min="0" name="price" class="form-control" value="<?php echo $price; ?>">
                             <span class="help-block"><?php echo $price_err;?></span>
                         </div>
-                        <div class="form-group <?php echo (!empty($prodname_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group <?php echo (!empty($img_err)) ? 'has-error' : ''; ?>">
                             <label>Image</label>
-                            <input type="file" name="img" class="form-control" value="<?php echo $img; ?>">
-                            <span class="help-block"><?php echo $img_err;?></span>
+                            <input type="file" class="form-control" name="files[]" id="files[5]" multiple value="<?php echo $img; ?>">
+                            <span class="help-block"><?php foreach ($img_err as $errMsg) { echo "$errMsg <br>"; }?></span>
                         </div>
-                        <input type="submit" class="btn btn-primary" value="Create New Product" style="background-color: #e04b11; border: none;">
+                        <input type="submit" class="btn btn-primary" name="submit" value="Add Product" style="background-color: #e04b11; border: none;">
                         <a href="manage.php" class="btn btn-default">Cancel</a>
                     </form>
                 </div>
-            </div>        
+            </div>
         </div>
     </div>
 </body>
