9f492a919f80f86732af1075455310a52b2288aa
s3757317 <s3757317@users.noreply.github.com>
2020-05-28 03:26:02 +0700

    removed code that does nothing

--- a5/includes/javascript.php
+++ a5/includes/javascript.php
@@ -1,25 +1,4 @@
 <script>
-  // Accordion 
-  function myAccFunc() {
-    var x = document.getElementById("demoAcc");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-
-  // Open and close sidebar
-  function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-  }
-
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
-
   /* When the user clicks on the button, 
   toggle between hiding and showing the dropdown content */
   function displayCategories() {
