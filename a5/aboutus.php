58a42c55c60cbe30a2b44c6816202a8409c2c5f1
s3757317 <s3757317@users.noreply.github.com>
2020-05-31 21:52:21 +0700

    fixed gap under footer

--- a5/aboutus.php
+++ a5/aboutus.php
@@ -21,34 +21,30 @@
     <link rel="icon" href="img/favicon.png">
 </head>
 <body>
-  <?php
-      include('includes/header.php');
-  ?>
+    <?php include('includes/header.php'); ?>
 
-  <div class="aboutus-container">
-      <div>
-          <h1><b><u>About Us</u></b></h1>
-          <br>
-          <div>
-              Zael is a global lifestyle brand that creates industrial inspired watches, eyewear, apparel and accessories for individuals around the world who live everyday like it’s Friday
-              <br>
-              <br>
-              Pushing and challenging the norms within the industry according to our own beliefs, dreams and experiences, Zael is a Life attitude
-              <br>
-              <br>
-              At Zael we're also passionate about offering our customers the very best prices on popular brand-name watches. We directly source top watches from around the globe. We, in turn, bring this directly to you, eliminating the middle man, providing savings to our valued customers. This gives us the buying power to offer authentic, brand new watches at the very best prices.
-              <br>
-              <br>
-              Join the community and share the #liveZael attitude!
-          </div>
-      </div>
-  </div>
+    <div class="aboutus-container">
+        <div>
+            <h1><b><u>About Us</u></b></h1>
+            <br>
+            <div>
+                Zael is a global lifestyle brand that creates industrial inspired watches, eyewear, apparel and accessories for individuals around the world who live everyday like it’s Friday
+                <br>
+                <br>
+                Pushing and challenging the norms within the industry according to our own beliefs, dreams and experiences, Zael is a Life attitude
+                <br>
+                <br>
+                At Zael we're also passionate about offering our customers the very best prices on popular brand-name watches. We directly source top watches from around the globe. We, in turn, bring this directly to you, eliminating the middle man, providing savings to our valued customers. This gives us the buying power to offer authentic, brand new watches at the very best prices.
+                <br>
+                <br>
+                Join the community and share the #liveZael attitude!
+            </div>
+        </div>
+    </div>
 
-  <div id="blankspace"></div>
+    <div id="blankspace"></div>
+    <div id="blankspace"></div>
 
-  <?php
-    include('includes/footer.php');
-    include('includes/javascript.php');
-  ?>
+    <?php include('includes/footer.php'); ?>
 </body>
 </html>
